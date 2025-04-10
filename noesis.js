a5f3029decb271e9480058e5bdf68fa6c722934f
Harsh <techbillharsh@gmail.com>
2025-04-10 14:38:11 +0530

    Initial commit

---
+++ noesis.js
@@ -0,0 +1,1 @@
+console.log("Welcome to noesis 💥📚");
---
+++ practice.js
@@ -0,0 +1,26 @@
+// 1. Variables
+let founder = "Harsh";
+let college = "XYZ College";
+
+// 2. Function
+function greet(user) {
+  console.log("Hello from Noesis, " + user + " 👋📚");
+}
+
+greet(founder);
+
+// 3. Object
+let user = {
+  name: "Harsh",
+  college: "XYZ College",
+  xp: 25
+};
+
+console.log("User XP:", user.xp);
+
+// 4. Array
+let clubMembers = ["Harsh", "Ananya", "Jay", "Zoya"];
+
+for (let i = 0; i < clubMembers.length; i++) {
+  console.log("👥 Member:", clubMembers[i]);
+}
